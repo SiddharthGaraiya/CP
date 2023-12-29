8249aa27974aacf169f2a80d94c74eab43a86946
SiddharthGaraiya <siddharth.garaiya01@gmail.com>
2023-12-29 10:36:31 +0530

    initial commit

---
+++ Codeforces/A_Team.cpp
@@ -0,0 +1,16 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int count=0;
+
+    while(n--){
+        int a,b,c; 
+        cin>>a>>b>>c;
+        if(a+b+c >=2) count++;
+    }
+    cout<<count<<endl;
+    return 0;
+}
