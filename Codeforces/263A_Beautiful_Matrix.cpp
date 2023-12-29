aac510144afedaee3d201a7bdb2a503fb55dd2ee
SiddharthGaraiya <siddharth.garaiya01@gmail.com>
2023-12-29 20:28:27 +0530

    263A_beautiful_matrix

---
+++ Codeforces/263A_Beautiful_Matrix.cpp
@@ -0,0 +1,12 @@
+    #include<bits/stdc++.h>
+    using namespace std;
+    int i,j,k,x;
+    int main(){
+        for(i=1;i<=5;i++)
+            for(j=1;j<=5;j++){
+                cin>>x;
+                if(x)
+                return  cout<<abs(3-i)+abs(3-j),0;
+        }
+        return 0;
+    }
