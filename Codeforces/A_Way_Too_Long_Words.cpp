9d1ae96a84bcb77f3fdb2df93b1708b525e9e3db
SiddharthGaraiya <siddharth.garaiya01@gmail.com>
2023-12-29 10:34:53 +0530

    initial commit

---
+++ Codeforces/A_Way_Too_Long_Words.cpp
@@ -0,0 +1,14 @@
+# include <bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int n;
+    string s;
+    cin>>n;
+    while (n--){
+        cin>>s;
+        if (s.size ()>10){
+            cout<<s[0]<<s.size()-2<<s.back()<<endl;         
+        }else cout<<s<<endl;
+    }
+}
