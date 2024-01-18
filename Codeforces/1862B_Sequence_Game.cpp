21403630d191898d1a3a5fa0ea20a125b4c4f025
SiddharthGaraiya <siddharth.garaiya01@gmail.com>
2024-01-18 19:14:54 +0530

    1862B - Sequence Game

---
+++ Codeforces/1862B_Sequence_Game.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ld long double
+#define ll long long
+#define pb push_back
+#define yes cout << "YES\n"
+#define no  cout << "NO\n"
+#define NEW cout<<"\n";
+#define forw(i , a , b) for (int i = (a); i <= (b); ++i)
+#define forq(i , b , a) for (int i = (a); i >= (b); --i)
+#define whm(n) while (n--)
+#define forr(i , b) forw( i , 0 , b - 1 )
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+
+
+void solve()
+{
+    int n;cin>>n;
+    int arr[n];
+
+    forr (i,n){
+        cin>>arr[i];
+    }
+    vector <int> v;
+    v.push_back(arr[0]);
+    forw(i,1,n-1){
+        if(arr[i]>=arr[i-1]) v.push_back(arr[i]);
+        else  {v.push_back(arr[i]);v.push_back(arr[i]);}
+    }
+    cout<<v.size();NEW
+    for (auto i :v){
+        cout<<i <<" ";
+    }NEW
+
+}
+int main() {
+
+    fast;
+    int t=1;
+    cin>>t;
+    while(t--)
+    {
+        // cout<<solve();NEW;
+        solve();
+        // if(solve()) yes; else no;
+    }
+}
