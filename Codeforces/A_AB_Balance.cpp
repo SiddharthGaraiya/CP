289e8322a1692367189301611b6fe02d52a065c8
SiddharthGaraiya <siddharth.garaiya01@gmail.com>
2024-08-03 21:08:01 +0530

    Create A_AB_Balance.cpp

---
+++ Codeforces/A_AB_Balance.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ld long double
+#define ll long long
+#define pb push_back
+#define yes cout << "YES\n"
+#define no  cout << "NO\n"
+#define NEW cout<<"\n";
+#define forw(i , a , b) for (int i = (a); i <= (b); ++i)
+#define forq(i , b , a) for (int i = (a); i >= (b); --i)
+#define whm(n) while (n--)
+#define forr(i , b) forw( i , 0 , b - 1 )
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+
+
+void solve()
+{
+    string s;
+    cin>>s;
+
+    if (s[0]==s[s.size()-1])
+        {cout<<s<<"\n";return; }
+    else 
+        cout<<s.substr(0,s.size()-1)<<s[0]<<"\n";
+}
+int main() {
+
+    fast;
+    int t=1;
+    cin>>t;
+    while(t--)
+    {
+        // cout<<solve();NEW;
+        solve();
+        // if(solve()) yes; else no;
+    }
+}
