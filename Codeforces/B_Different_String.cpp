81b1a9e9696e390b9cb0deb3b3cabe57539c993e
SiddharthGaraiya <siddharth.garaiya01@gmail.com>
2024-08-20 11:52:32 +0530

    Create B_Different_String.cpp

---
+++ Codeforces/B_Different_String.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ld long double
+#define ll long long
+#define pb push_back
+#define yes cout << "YES\n"
+#define no  cout << "NO\n"
+#define NEW cout<<"\n";
+#define forw(i , a , b) for (int i = (a); i <= (b); ++i)
+#define forq(i , b , a) for (int i = (a); i >= (b); --i)
+#define whm(n) while (n--)
+#define forr(i , b) forw( i , 0 , b - 1 )
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+
+
+void solve()
+{
+    string s;
+    cin>>s;
+    char c= s[0];
+    for (int i =1;i<s.size();i++){
+        if (s[i]!=c){
+            char temp =s[i];
+            s[i]=c;
+            s[0] =temp;
+            cout<<"YES\n";
+            cout<<s<<"\n";
+            return;
+        }
+    }
+    cout<<"NO\n";
+
+}
+int main() {
+
+    fast;
+    int t=1;
+    cin>>t;
+    while(t--)
+    {
+        // cout<<solve();NEW;
+        solve();
+        // if(solve()) yes; else no;
+    }
+}
