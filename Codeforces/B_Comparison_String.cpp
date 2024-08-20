b79ab176f4d08abd051785b19821cfabbf529b78
SiddharthGaraiya <parthivshekhliya@gmail.com>
2024-08-20 11:41:25 +0530

    Create B_Comparison_String.cpp

---
+++ Codeforces/B_Comparison_String.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ld long double
+#define ll long long
+#define pb push_back
+#define yes cout << "YES\n"
+#define no  cout << "NO\n"
+#define NEW cout<<"\n";
+#define forw(i , a , b) for (int i = (a); i <= (b); ++i)
+#define forq(i , b , a) for (int i = (a); i >= (b); --i)
+#define whm(n) while (n--)
+#define forr(i , b) forw( i , 0 , b - 1 )
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+
+
+void solve()
+{
+    int n;cin>>n;
+    
+    string s;cin>>s;
+
+
+    int currLength=1;
+    int maxLength=1;
+    for (int i=0;i<n-1;i++){
+        while (s[i]==s[i+1]) {
+            currLength++;
+            i++;
+        }
+        maxLength=max(maxLength,currLength);
+        currLength=1;
+    }
+    cout<<maxLength+1<<"\n";
+}
+int main() {
+
+    fast;
+    int t=1;
+    cin>>t;
+    while(t--)
+    {
+        // cout<<solve();NEW;
+        solve();
+        // if(solve()) yes; else no;
+    }
+}
