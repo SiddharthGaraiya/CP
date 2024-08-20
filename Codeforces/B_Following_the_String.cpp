4d6acc0b822c6dd3a70c51940505574e5f993bd1
SiddharthGaraiya <siddharth.garaiya01@gmail.com>
2024-08-20 11:53:29 +0530

    Create B_Following_the_String.cpp

---
+++ Codeforces/B_Following_the_String.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ld long double
+#define ll long long
+#define pb push_back
+#define yes cout << "YES\n"
+#define no  cout << "NO\n"
+#define NEW cout<<"\n";
+#define forw(i , a , b) for (int i = (a); i <= (b); ++i)
+#define forq(i , b , a) for (int i = (a); i >= (b); --i)
+#define whm(n) while (n--)
+#define forr(i , b) forw( i , 0 , b - 1 )
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+
+void solve()
+{
+    int v[200010]={0};   
+    int n;cin>>n;
+    int a;
+    string s="";
+    while (n--){
+        cin>>a;
+        
+        s.push_back(v[a]+'a');
+        v[a]++;
+    }
+    cout<<s<<"\n";
+}
+int main() {
+
+    fast;
+    int t=1;
+    cin>>t;
+    while(t--)
+    {
+        // cout<<solve();NEW;
+        solve();
+        // if(solve()) yes; else no;
+    }
+}
